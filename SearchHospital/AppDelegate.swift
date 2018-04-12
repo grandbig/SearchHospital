a7c8c28680d88ce8c244d8beb9f0a8b481116b3f
Takahiro Kato <takahiro.kato15@gmail.com>
2018-04-12 10:47:24 +0900

    refs #1, #2 plistに追加したapiKeyの読み込み処理を実装 基本的なディレクトリ構成を追加

--- SearchHospital/AppDelegate.swift
+++ SearchHospital/AppDelegate.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import GoogleMaps
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -15,7 +16,15 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        
+        if let path = Bundle.main.path(forResource: "key", ofType: "plist") {
+            if let dic = NSDictionary(contentsOfFile: path) as? [String: Any] {
+                if let apiKey = dic["googleApiKey"] as? String {
+                    GMSServices.provideAPIKey(apiKey)
+                }
+            }
+        }
+        
         return true
     }
 
